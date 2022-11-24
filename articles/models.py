0683cc8223a7c790fa883919f15b1481ae4ac7f0
naerumii <github>
2022-11-24 16:28:26 +0900

    [추가] 테스트를 위한 articles, user 모델 생성

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
+class Image(models.Model):
+    output_img = models.FileField("결과사진", upload_to="output/", null=True)
+    
+class Article(models.Model):
+    user = models.ForeignKey(User, verbose_name="작성자", on_delete=models.CASCADE)
+    content = models.TextField()
+    title = models.CharField("제목", max_length=50)
+    price = models.CharField("가격", max_length=50)
+    image = models.CharField(max_length=120)
+    created_at = models.DateTimeField("등록 일자", auto_now_add=True)
+    updated_at = models.DateTimeField("수정 일자", auto_now=True)
+    
+    def __str__(self):
+        return str(self.title)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,69 @@
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser, PermissionsMixin
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    # 일반 user 생성
+    def create_user(self, username, email, password=None):
+
+        if not username:
+            raise ValueError('Users must have an username')
+
+        user = self.model(
+            username = username,
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+    def create_superuser(self, username, email, password=None):
+        user = self.create_user(
+            username=username,
+            email=email,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(verbose_name="email", max_length=255, blank=True, null=True)
+    username = models.CharField(verbose_name="username", max_length=20, blank=False, unique=True)
+    
+    
+    # User 모델의 필수 field
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    
+    # 헬퍼 클래스 사용
+    objects = UserManager()
+    
+    #사용자의 USERUSERNAME_FIELD를 지정
+    USERNAME_FIELD = 'username'
+    # 필수로 작성해야하는 field
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return self.username
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
